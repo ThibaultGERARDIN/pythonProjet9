7b9ae85429fc1c12ec775ad7a0f43c7897da0452
Thibault G <thibault.gerardin@gmail.com>
2024-12-20 17:15:34 +0100

    model usermanager

--- litrevu/authentication/models.py
+++ litrevu/authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password=None, **extra_fields):
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, password, **extra_fields)
