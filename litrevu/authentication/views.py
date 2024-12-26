8323163c236fc08303adfb94f55ef9b8427145aa
ThibaultGerardin <thibault.gerardin@gmail.com>
2024-12-26 19:21:24 +0100

    ajout landing page et authentication form

--- litrevu/authentication/views.py
+++ litrevu/authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def landing(request):
+    form = UserCreationForm()
+    return render(request, 'authentication/landing.html', {'form': form})
